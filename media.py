74f7505e32c3b39f64529a0fe0d15d7fdc998446
LDRgabryele <pamella.silva11@aluno.ce.gov.br>
2023-10-30 14:10:46 -0300

    first commit

---
+++ media.py
@@ -0,0 +1,6 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+
+#utilizar o for para que consiga pecorrer entre numero 1 - 50 
+for media in range(1,51):
+    if media % 2 == 0: 
+        print (media)
